9d96dba98e6ada68e5724ef79f2ad5cb1e76fa68
Leo June Bedeo <jfljfb@gmail.com>
2021-07-25 17:02:39 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+function adminer_object() {
+	include_once "./plugins/plugin.php";
+	include_once "./plugins/login-password-less.php";
+	return new AdminerPlugin(array(
+		// TODO: inline the result of password_hash() so that the password is not visible in source codes
+		new AdminerLoginPasswordLess(password_hash("123", PASSWORD_DEFAULT)),
+	));
+}
+
+include "./adminer-4.8.1.php";
